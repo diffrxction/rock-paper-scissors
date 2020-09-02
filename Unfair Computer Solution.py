d2c435bddbcd991ec8bf78608ccace04f040c462
Aryan Pareek <65293175+diffrxction@users.noreply.github.com>
2020-09-03 05:13:49 +0530

    Create Unfair Computer Solution.py
    Solution for Unfair Computer problem.

---
+++ Unfair Computer Solution.py
@@ -0,0 +1,5 @@
+user_choice = input()
+choice = ['rock', 'paper', 'scissors']
+kryptonite = ['paper', 'scissors', 'rock']
+choiceindex = choice.index(user_choice)
+print("Sorry, but the computer chose "+ kryptonite[choiceindex])
